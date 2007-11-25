b745309356efc0fdc9d1eef301e5ebb322b416b9
chrisleung <chrisleung@3ea256b2-6675-11de-a9b1-77b8bb241e6b>
2007-11-25 10:58:24 +0000

    Added basic source and UI

---
+++ src/powerreader/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Main.java
+ *
+ * Created on November 24, 2007, 7:09 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package powerreader;
+
+/**
+ *
+ * @author cleung
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
