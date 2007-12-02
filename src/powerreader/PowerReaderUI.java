0a8d825ff248468eb14f36d68720d47d3fa39e5f
chrisleung <chrisleung@3ea256b2-6675-11de-a9b1-77b8bb241e6b>
2007-12-02 06:13:41 +0000

    Updated the UI and added J3D Canvas

--- src/powerreader/PowerReaderUI.java
+++ src/powerreader/PowerReaderUI.java
@@ -6,16 +6,102 @@
 
 package powerreader;
 
+// Import J3D Stuff
+import com.sun.j3d.utils.behaviors.mouse.MouseRotate;
+import com.sun.j3d.utils.geometry.ColorCube;
+import com.sun.j3d.utils.geometry.Text2D;
+import com.sun.j3d.utils.universe.*;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GraphicsConfiguration;
+import javax.media.j3d.*;
+import javax.vecmath.Color3f;
+import javax.vecmath.Vector3f;
+
 /**
  *
  * @author  cleung
  */
 public class PowerReaderUI extends javax.swing.JFrame {
     
+    // Manually added variables
+    private Canvas3D theCanvas;
+    
     /** Creates new form PowerReaderUI */
     public PowerReaderUI() {
         initComponents();
+        
+        // Now initialize the 3D Canvas
+        create3dCanvas();
     }
+    
+    private void create3dCanvas() {
+        
+        GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
+        
+        // construct the 3D image
+        Canvas3D canvas3D = new Canvas3D(config);
+        SimpleUniverse simpleU = new SimpleUniverse(canvas3D);
+        BranchGroup scene = createSceneGraph();
+        simpleU.getViewingPlatform().setNominalViewingTransform();       // This will move the ViewPlatform back a bit so the
+        simpleU.addBranchGraph(scene);
+        
+        m_panel_textArea.setLayout( new BorderLayout() );
+        m_panel_textArea.setOpaque( false );
+        m_panel_textArea.add("Center", canvas3D);   // <-- HERE IT IS - tada! j3d in swing
+    }
+    
+    private BranchGroup createSceneGraph() {
+        // Create the root of the branch graph
+        BranchGroup objRoot = new BranchGroup();
+        TransformGroup root_group = new TransformGroup(  );
+        root_group.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE); // allow the mouse behavior to rotate the scene
+        root_group.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+        objRoot.addChild( root_group );  // this is the local origin  - everyone hangs off this - moving this move every one
+        
+        root_group.addChild( new ColorCube(0.2) );  // add a color cube
+        root_group.addChild( new Label3D( 0.2f, 0.2f, 0.0f, "ColorCube") );
+        
+        MouseRotate mouseRotate = new MouseRotate( root_group );  // add the mouse behavior
+        mouseRotate.setSchedulingBounds( new BoundingSphere() );
+        objRoot.addChild( mouseRotate);
+        return objRoot;
+    }
+    
+    class Label3D
+            extends TransformGroup {
+        
+        public Label3D( float x, float y, float z, String msg ) {
+            super();
+            
+            // place it in the scene graph
+            Transform3D offset = new Transform3D();
+            offset.setTranslation( new Vector3f( x, y, z ));
+            this.setTransform( offset );
+            
+            // face it in the scene graph
+            Transform3D rotation = new Transform3D();
+            TransformGroup rotation_group = new TransformGroup( rotation );
+            this.addChild( rotation_group );
+            
+            // make a texture mapped polygon
+            Text2D msg_poly = new Text2D( msg, new
+                    Color3f( 1.0f, 1.0f, 1.0f),
+                    "Helvetica", 18, Font.PLAIN );
+            
+            
+            // set it to draw both the front and back of the poly
+            PolygonAttributes msg_attributes = new PolygonAttributes();
+            msg_attributes.setCullFace( PolygonAttributes.CULL_NONE );
+            msg_attributes.setBackFaceNormalFlip( true );
+            msg_poly.getAppearance().setPolygonAttributes( msg_attributes );
+            
+            // attach it
+            rotation_group.addChild( msg_poly );
+        }
+        
+    }
+    
     
     /** This method is called from within the constructor to
      * initialize the form.
@@ -24,7 +110,6 @@ public class PowerReaderUI extends javax.swing.JFrame {
      */
     // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
     private void initComponents() {
-        m_canvas_text = new java.awt.Canvas();
         m_button_play = new javax.swing.JButton();
         m_buton_stop = new javax.swing.JButton();
         m_slider_readSpeed = new javax.swing.JSlider();
@@ -42,13 +127,12 @@ public class PowerReaderUI extends javax.swing.JFrame {
         m_checkBox_showImages = new javax.swing.JCheckBox();
         m_checkBox_wordsGrow = new javax.swing.JCheckBox();
         m_checkBox_speechEnabled = new javax.swing.JCheckBox();
-        jMenuBar1 = new javax.swing.JMenuBar();
+        m_panel_textArea = new javax.swing.JPanel();
+        m_menubar = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Power Reader Alpha");
-        m_canvas_text.setBackground(new java.awt.Color(200, 200, 200));
-
         m_button_play.setText("Play");
         m_button_play.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -112,23 +196,36 @@ public class PowerReaderUI extends javax.swing.JFrame {
         m_checkBox_speechEnabled.setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0, 0, 0));
         m_checkBox_speechEnabled.setMargin(new java.awt.Insets(0, 0, 0, 0));
 
-        jMenu1.setText("&File");
+        //Test
+        org.jdesktop.layout.GroupLayout m_panel_textAreaLayout = new org.jdesktop.layout.GroupLayout(m_panel_textArea);
+        m_panel_textArea.setLayout(m_panel_textAreaLayout);
+        m_panel_textAreaLayout.setHorizontalGroup(
+            m_panel_textAreaLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(0, 436, Short.MAX_VALUE)
+        );
+        m_panel_textAreaLayout.setVerticalGroup(
+            m_panel_textAreaLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(0, 552, Short.MAX_VALUE)
+        );
+
+        jMenu1.setMnemonic('F');
+        jMenu1.setText("File");
         jMenu1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenu1ActionPerformed(evt);
             }
         });
 
-        jMenuBar1.add(jMenu1);
+        m_menubar.add(jMenu1);
 
-        setJMenuBar(jMenuBar1);
+        setJMenuBar(m_menubar);
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
-                .add(m_canvas_text, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .add(m_panel_textArea, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
                     .add(org.jdesktop.layout.GroupLayout.LEADING, layout.createSequentialGroup()
@@ -194,27 +291,27 @@ public class PowerReaderUI extends javax.swing.JFrame {
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(m_checkBox_speechEnabled)
                 .addContainerGap(49, Short.MAX_VALUE))
-            .add(m_canvas_text, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .add(m_panel_textArea, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
+    
     private void m_button_hlColorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_m_button_hlColorActionPerformed
 // TODO add your handling code here:
     }//GEN-LAST:event_m_button_hlColorActionPerformed
-
+    
     private void m_button_bgColorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_m_button_bgColorActionPerformed
 // TODO add your handling code here:
     }//GEN-LAST:event_m_button_bgColorActionPerformed
-
+    
     private void m_button_fgColorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_m_button_fgColorActionPerformed
 // TODO add your handling code here:
     }//GEN-LAST:event_m_button_fgColorActionPerformed
-
+    
     private void m_button_playActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_m_button_playActionPerformed
 // TODO add your handling code here:
     }//GEN-LAST:event_m_button_playActionPerformed
-
+    
     private void jMenu1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenu1ActionPerformed
 // TODO add your handling code here:
     }//GEN-LAST:event_jMenu1ActionPerformed
@@ -232,13 +329,11 @@ public class PowerReaderUI extends javax.swing.JFrame {
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JButton m_buton_stop;
     private javax.swing.JButton m_button_bgColor;
     private javax.swing.JButton m_button_fgColor;
     private javax.swing.JButton m_button_hlColor;
     private javax.swing.JButton m_button_play;
-    private java.awt.Canvas m_canvas_text;
     private javax.swing.JCheckBox m_checkBox_showImages;
     private javax.swing.JCheckBox m_checkBox_speechEnabled;
     private javax.swing.JCheckBox m_checkBox_wordsGrow;
@@ -248,9 +343,12 @@ public class PowerReaderUI extends javax.swing.JFrame {
     private javax.swing.JLabel m_label_lod;
     private javax.swing.JLabel m_label_readSpeed;
     private javax.swing.JLabel m_label_zoomLevel;
+    private javax.swing.JMenuBar m_menubar;
+    private javax.swing.JPanel m_panel_textArea;
     private javax.swing.JSlider m_slider_lod;
     private javax.swing.JSlider m_slider_readSpeed;
     private javax.swing.JSlider m_slider_zoomLevel;
     // End of variables declaration//GEN-END:variables
+    
     
 }
